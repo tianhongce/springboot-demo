c697a215dcc7d6cce64af49cd52f5d2975cc17e4
tianhongce <1196585084@qq.com>
2020-02-19 20:26:20 +0800

    测试(document).ready

--- src/main/java/com/me/springbootdemo/controller/HelloController.java
+++ src/main/java/com/me/springbootdemo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.me.springbootdemo.controller;
 
+import com.me.springbootdemo.entity.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello(Model model){
 //        Student student = new Student("11","name");
-//        model.addAttribute("student",student);
+        Student student = new Student();
+        student.setAge(3);
+        student.setId("21");
+        student.setName("ba");
+        model.addAttribute("student",student);
+        // TODO: 测试document.ready()前端
         return "hello";
     }
 }
